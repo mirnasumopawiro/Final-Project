2bc0a9f995e6b1d8c9c793c972d907f0e208b243
Mirna Sumopawiro <mirnasumopawiro@Rudys-MacBook-Pro.local>
2016-10-18 16:13:53 +0700

    second update

--- Final Project/Final Project/main.cpp
+++ Final Project/Final Project/main.cpp
@@ -9,6 +9,12 @@
 #include <iostream>
 using namespace std;
 
+void showMenu();
+
+char choice = ' ';
+int order1;
+int orderAmount1;
+
 int main() {
     
     const float BOOKCASE_PRICE = 40.91;
@@ -18,34 +24,36 @@ int main() {
     const float WARDROBE_PRICE = 172.72;
     const float CALL_OUT_FEE = 31.82;
     const float REMOVE_PACKAGE = 13.64;
-    int order1;
     int orderAmount1;
-    char choice;
+   
     
     cout << "Welcome to Install4U\n";
     
-    do {
-        choice = ' ';
-        cout << "Which furniture would you like us to install for you? (ex: for Bookcase, enter 1)\n";
-        cout << "1. Bookcase \n";
-        cout << "2. Bed with Under Storage \n";
-        cout << "3. Bed without Under Storage \n";
-        cout << "4. TV Bench \n";
-        cout << "5. 2-frame Wardrobe with Hinged Doors \n";
-        cout << "Order: ";
-        cin >> order1;
-        
-        cout << "How much of this would you like to install?\n";
-        cin >> orderAmount1;
-        
-        cout << "Would you like to add more items to your cart? (Y/N) \n";
-        cin >> choice;
-    } while (choice == 'Y' || choice == 'y'  );
-    
-    
-    
+            showMenu ();
     
     return 0;
 }
 
+void showMenu()
+{
+    
+    do
+    {
 
+    cout << "Which furniture would you like us to install for you? (ex: for Bookcase, enter 1)\n";
+    cout << "1. Bookcase \n";
+    cout << "2. Bed with Under Storage \n";
+    cout << "3. Bed without Under Storage \n";
+    cout << "4. TV Bench \n";
+    cout << "5. 2-frame Wardrobe with Hinged Doors \n";
+    cout << "Order: ";
+    cin >> order1;
+    
+    cout << "How much of this would you like to install?\n";
+    cin >> orderAmount1;
+    
+    cout << "Would you like to add more items to your cart? (Y/N) \n";
+    cin >> choice;
+    }
+    while (choice == 'Y' || choice == 'y');
+}
